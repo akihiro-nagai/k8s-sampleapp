99ea335ec71165807dabc94c562a0fb48f26e0bc
Akihiro Nagai <akihiro.nagai.curry@gmail.com>
2022-04-21 11:02:22 +0900

    cleanup

--- terraform/temp/workload-identity.tf
+++ terraform/temp/workload-identity.tf
@@ -7,18 +7,18 @@ locals {
   }
 }
 
-module "testpod-workload-identity" {
+module "workload-identity-test" {
   for_each = local.config_map
 
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   name                = "${each.key}-workload-identity"
   project_id          = "maugram-dev"
-  cluster_name        = "cluster-0223"
+  cluster_name        = "cluster-0315"
   location            = "us-central1-c"
   namespace           = "dev"
   gcp_sa_name         = each.value.gsa_name
   k8s_sa_name         = each.value.ksa_name
   use_existing_k8s_sa = true
-  roles               = []
-  #roles      = ["roles/cloudsql.client", "roles/autoscaling.metricsWriter", "roles/cloudtrace.agent", "roles/logging.logWriter", "roles/monitoring.metricWriter"]
+  annotate_k8s_sa     = false
+  roles               = ["roles/cloudsql.client", "roles/storage.admin"]
 }
